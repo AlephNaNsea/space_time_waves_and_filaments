426dcf37a11308270d0a89b2eaef9cf923847a9c
Chico <olaguer.chicoandre@gmail.com>
2026-05-06 20:13:22 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_AlephNaNsea_space_time_waves_and_filaments user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
